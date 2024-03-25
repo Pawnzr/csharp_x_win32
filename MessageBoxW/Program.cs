bccce01ab4bd3715b801c66397da0eaa5b82e866
Pawn3r <anduu150620@gmail.com>
2024-03-25 14:47:09 +0700

    messageBoxW() win32

---
+++ MessageBoxW/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+namespace MessageBox
+{
+    internal class Program
+    {
+        [DllImport("user32.dll")]
+        /*int MessageBoxW(
+          [in, optional] HWND hWnd,
+          [in, optional] LPCWSTR lpText,
+          [in, optional] LPCWSTR lpCaption,
+          [in] UINT uType
+        );*/
+
+        public static extern int MessageBoxW(
+            IntPtr hWnd,
+            [param: MarshalAs(UnmanagedType.LPWStr)] string lpText,
+            [param: MarshalAs(UnmanagedType.LPWStr)] string lpTitle,
+            UInt32 uType
+            );
+
+        static void Main(string[] args)
+        {
+            int box = MessageBoxW(IntPtr.Zero, "Hello Pwnzr", "This is title", 0);
+            Console.ReadKey();
+        }
+    }
+}
